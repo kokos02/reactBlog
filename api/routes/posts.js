d2fa47dbbf5edcc1a49de22dca625bb7b2ed3dc9
Nikos <eukprice@gmail.com>
2023-01-21 12:51:07 +0200

    Added axios to fetch data

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -1,5 +1,18 @@
 import express from "express";
+import {
+    getPosts,
+    getPost,
+    addPost,
+    updatePost,
+    deletePost,
+} from "../controllers/post.js";
 
 const router = express.Router();
+
+router.get("/", getPosts);
+router.get("/:id", getPost);
+router.get("/", addPost);
+router.get("/:id", deletePost);
+router.get("/id", updatePost);
 
 export default router;
